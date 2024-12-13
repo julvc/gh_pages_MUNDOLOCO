03603893e11ba38f42b3f17ce45c7026a3deb901
Julio Varas Contreras <jvarascontreras@gmail.com>
2024-12-13 15:31:34 -0300

    Se agrega cambio para forzar la url de base para cargar blog

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/gh_pages_MUNDOLOCO/'
+  base: '/gh_pages_MUNDOLOCO/',
+  define: {
+    'process.env.VITE_API_URL': JSON.stringify(process.env.VITE_API_URL),
+  },
 })
